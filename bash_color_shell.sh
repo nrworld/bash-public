8361b21e97ce44b17b0d4a20c467950703186e78
Nikhil Raj <5545551+nrworld@users.noreply.github.com>
2023-11-20 11:30:39 -0500

    Create bash_color_shell.sh

---
+++ bash_color_shell.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+wget https://raw.githubusercontent.com/nrworld/bash-public/main/bashrc
+wget https://raw.githubusercontent.com/nrworld/bash-public/main/profile
+mv bashrc ~/.bashrc
+mv profile ~/.profile
